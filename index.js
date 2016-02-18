4f77cb6a6c0078cf8df2813172069d095a671ac2
周鹏 <zhoupeng@zhoupengdeMacBook-Pro.local>
2016-02-18 20:03:13 +0800

    init

---
+++ index.js
@@ -0,0 +1,34 @@
+var gutil = require('gulp-util'),
+    through = require('through2'),
+    postcss = require('postcss'),
+    configReader = require('./lib/configReader'),
+    configInstant = require('./lib/configInstant');
+
+module.exports = function(options) {
+    var configration = {},
+        cachedConfig = '',
+        readyStatus = false;
+
+        
+
+    return through.obj(function(file, enc, cb) {
+        if (file.isNull()) {
+            this.push(file);
+            return cb();
+        }
+        if (file.isStream()) {
+            this.emit('error', new gutil.PluginError(PLUGIN_NAME, 'Streaming not supported'));
+            return cb();
+        }
+        var me = this;
+        configReader(options, null, function(data) {
+            configration = data;
+            var content = configInstant(file, configration);
+            me.push(file);
+            cb();
+        });
+            
+        
+        
+    });
+};
